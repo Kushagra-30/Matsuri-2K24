20eabebdb657a70089aeec51cc2b9ba996d870a4
Kushagra-30 <kushagras104@gmail.com>
2024-10-12 10:54:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        matsuri: ["matsuri", "sans-serif"],
+        mahaveeram1: ["mahaveeram1", "sans-serif"],
+        mahaveeram2: ["mahaveeram2", "sans-serif"],
+        bodyfont: ["bodyfont", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
